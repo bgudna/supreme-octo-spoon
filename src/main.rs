5488ecf67f87f4ed654cced1f91d7d980c78a44e
bgudna <b@bgudna.com>
2022-09-10 14:45:26 +0200

    adding a field of view system

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,8 @@ mod player;
 pub use player::*;
 mod rect;
 pub use rect::Rect;
+mod visibility_system;
+use visibility_system::VisibilitySystem;
 
 
 pub struct State {
@@ -17,6 +19,8 @@ pub struct State {
 
 impl State {
     fn run_systems(&mut self) {
+        let mut vis = VisibilitySystem{};
+        vis.run_now(&self.ecs);
         self.ecs.maintain();
     }
 }
@@ -50,6 +54,7 @@ fn main() -> rltk::BError {
     gs.ecs.register::<Position>();
     gs.ecs.register::<Renderable>();
     gs.ecs.register::<Player>();
+    gs.ecs.register::<Viewshed>();
 
     let map : Map = Map::new_map_rooms_and_corridors();
     let (player_x, player_y) = map.rooms[0].center();
@@ -64,6 +69,7 @@ fn main() -> rltk::BError {
             bg: RGB::named(rltk::BLACK),
         })
         .with(Player{})
+        .with(Viewshed{ visible_tiles : Vec::new(), range : 8, dirty: true})
         .build();
 
     //gs.ecs.insert(new_map_rooms_and_corridors());    
--- src/map.rs
+++ src/map.rs
@@ -1,4 +1,6 @@
-use rltk::{ RGB, Rltk, RandomNumberGenerator };
+use rltk::{ RGB, Rltk, RandomNumberGenerator, Algorithm2D, BaseMap, Point };
+use crate::{Viewshed, Player};
+
 use super::{Rect};
 use std::cmp::{ max, min };
 use specs::prelude::*;
@@ -13,7 +15,9 @@ pub struct Map {
     pub tiles : Vec<TileType>,
     pub rooms : Vec<Rect>,
     pub width : i32,
-    pub height: i32
+    pub height: i32,
+    pub revealed_tiles : Vec<bool>,
+    pub visible_tiles : Vec<bool>
 }
 
 impl Map {
@@ -55,7 +59,9 @@ impl Map {
             tiles : vec![TileType::Wall; 80*50],
             rooms : Vec::new(),
             width: 80,
-            height: 50
+            height: 50,
+            revealed_tiles : vec![false; 80*50],
+            visible_tiles : vec![false; 80*50]
         };
 
         const MAX_ROOMS : i32 = 30;
@@ -99,29 +105,50 @@ impl Map {
 
 }
 
+impl BaseMap for Map {
+    fn is_opaque(&self, idx:usize) -> bool {
+        self.tiles[idx as usize] == TileType::Wall
+    }
+}
 
+impl Algorithm2D for Map {
+    fn dimensions(&self) -> Point {
+        Point::new(self.width, self.height)
+    }
+}
 
 pub fn draw_map(ecs: &World, ctx: &mut Rltk) {
-
+    //let mut viewsheds = ecs.write_storage::<Viewshed>();
+    //let mut players = ecs.write_storage::<Player>();
     let map = ecs.fetch::<Map>();
 
     let mut y = 0;
     let mut x = 0;
 
     for (idx, tile) in map.tiles.iter().enumerate() {
-        match tile {
-            TileType::Floor => {
-                ctx.set(x, y, RGB::from_f32(0.5, 0.5, 0.5), RGB::from_f32(0., 0., 0.), rltk::to_cp437('.'));
+        
+        if map.revealed_tiles[idx] {
+            let glyph;
+            let mut fg;
+            match tile {
+                TileType::Floor => {
+                    glyph = rltk::to_cp437('.');
+                    fg = RGB::from_f32(0.5, 0.5, 0.5);
+                }
+                TileType::Wall => {
+                    glyph = rltk::to_cp437('#');
+                    fg = RGB::from_f32(0.0, 1.0, 0.0);
+                }
             }
-            TileType::Wall => {
-                ctx.set(x, y, RGB::from_f32(0.0, 1.0, 0.0), RGB::from_f32(0., 0., 0.), rltk::to_cp437('#'));
+            if !map.visible_tiles[idx] {
+                fg = fg.to_greyscale()
             }
+            ctx.set(x, y, fg, RGB::from_f32(0., 0., 0.), glyph);
         }
-
         x += 1;
         if x > 79 {
             x = 0;
             y += 1;
-        }
+        }        
     }
 }
